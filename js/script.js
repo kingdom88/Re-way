43ebd5b9d70ad5334da365802c49f46faae8681f
kingdom88 <kingdomlwandle@gmail.com>
2022-03-16 02:07:20 +0200

    My first commit

---
+++ js/script.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+    $('.js-example-basic-multiple').select2();
+
+    $(".closed").click(function () {
+        $(".card-1").hide(1000);
+    });
+
+    $(".fa-times").click(function () {
+        $(".card-4").hide(1000);
+    });
+});
+
+var lowerSlider = document.querySelector('#lower');
+var upperSlider = document.querySelector('#upper');
+
+document.querySelector('#two').value = upperSlider.value;
+document.querySelector('#one').value = lowerSlider.value;
+
+var lowerVal = parseInt(lowerSlider.value);
+var upperVal = parseInt(upperSlider.value);
+
+upperSlider.oninput = function () {
+    lowerVal = parseInt(lowerSlider.value);
+    upperVal = parseInt(upperSlider.value);
+
+    if (upperVal < lowerVal + 4) {
+        lowerSlider.value = upperVal - 4;
+        if (lowerVal == lowerSlider.min) {
+            upperSlider.value = 4;
+        }
+    }
+    document.querySelector('#two').value = this.value
+};
+
+lowerSlider.oninput = function () {
+    lowerVal = parseInt(lowerSlider.value);
+    upperVal = parseInt(upperSlider.value);
+    if (lowerVal > upperVal - 4) {
+        upperSlider.value = lowerVal + 4;
+        if (upperVal == upperSlider.max) {
+            lowerSlider.value = parseInt(upperSlider.max) - 4;
+        }
+    }
+    document.querySelector('#one').value = this.value
+};
+
+
+// carousel
+$("[id^=carousel-thumbs]").carousel({
+    interval: false
+});
+
+/** Pause/Play Button **/
+$(".carousel-pause").click(function () {
+    var id = $(this).attr("href");
+    if ($(this).hasClass("pause")) {
+        $(this).removeClass("pause").toggleClass("play");
+        $(this).children(".sr-only").text("Play");
+        $(id).carousel("pause");
+    } else {
+        $(this).removeClass("play").toggleClass("pause");
+        $(this).children(".sr-only").text("Pause");
+        $(id).carousel("cycle");
+    }
+    $(id).carousel;
+});
+
+
+$(".carousel-fullscreen").click(function () {
+    var id = $(this).attr("href");
+    $(id).find(".active").ekkoLightbox({
+        type: "image"
+    });
+});
+
+if ($("[id^=carousel-thumbs] .carousel-item").length < 2) {
+    $("#carousel-thumbs [class^=carousel-control-]").remove();
+    $("#carousel-thumbs").css("padding", "0 5px");
+}
+
+$("#carousel").on("slide.bs.carousel", function (e) {
+    var id = parseInt($(e.relatedTarget).attr("data-slide-number"));
+    var thumbNum = parseInt(
+        $("[id=carousel-selector-" + id + "]")
+            .parent()
+            .parent()
+            .attr("data-slide-number")
+    );
+    $("[id^=carousel-selector-]").removeClass("selected");
+    $("[id=carousel-selector-" + id + "]").addClass("selected");
+    $("#carousel-thumbs").carousel(thumbNum);
+});
